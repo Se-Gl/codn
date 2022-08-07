a756fe85552faf6ad98d0a7e76db90ad036885c3
Se-Gl <me@severinglaser.ch>
2022-08-07 17:03:37 +0200

    fix(modal): added modal icon color class name

--- src/components/Modal/CloseIcon.js
+++ src/components/Modal/CloseIcon.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../../../dist/style/green.css'
 
-function CloseIcon({ width = '20', height = '20' }) {
+function CloseIcon({ width = '20', height = '20', iconColor = 'fill-gray' }) {
   return (
     <svg
       className='cursor-pointer'
@@ -11,7 +11,7 @@ function CloseIcon({ width = '20', height = '20' }) {
       fill='none'
       viewBox='0 0 20 20'>
       <path
-        className='fill-gray'
+        className={iconColor}
         d='M5.187 4.01A.833.833 0 004.01 5.187L8.822 10l-4.814 4.813a.835.835 0 101.179 1.178L10 11.178l4.813 4.813a.833.833 0 001.178-1.178L11.178 10l4.813-4.812a.833.833 0 00-1.178-1.179L10 8.822 5.187 4.008v.001z'></path>
     </svg>
   )
